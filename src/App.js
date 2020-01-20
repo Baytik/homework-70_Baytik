86508c53724dd2524bd12ef1844b000ed3cdbd62
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-01-20 22:53:02 +0600

    Создал state получил данные из firebase и вывел данные в теги

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Header from "./Components/Header/Header";
 import axiosAPI from "./axiosAPI";
 
 const App = () => {
-
     const initialShows = [];
-    const initialText = '';
     const [shows, setShows] = useState(initialShows);
+    const initialText = '';
     const [text, setText] = useState(initialText);
     console.log(shows);
 
@@ -15,6 +14,7 @@ const App = () => {
       const response = await axiosAPI.get();
       setShows(response.data);
     };
+
     useEffect( () => {
         fetchData();
     }, []);
@@ -27,9 +27,19 @@ const App = () => {
                   <p className="main-title">Search for TV Show</p>
               </div>
               <div>
-                  <input type="text"/>
+                  <input type="text" onChange={(e) => setText(e.target.value)} value={text}/>
               </div>
           </div>
+                <div className="blocks">
+              {Object.keys(shows).map(k => (
+                  <div key={k}>
+                    <div className="block">
+                       <p>{shows[k].show.name}</p>
+                      <img src={shows[k].show.image.medium} alt="images"/>
+                    </div>
+                  </div>
+              ))}
+                </div>
       </div>
   )
 };
