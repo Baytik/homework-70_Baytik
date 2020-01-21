0673cf5436b3541dd02ba2b3dd09b685f20bee4d
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-01-21 18:47:35 +0600

    ЗАкончил задание

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from "./Components/Header/Header";
 import axiosAPI from "./axiosAPI";
+import nanoid from 'nanoid';
 
 const App = () => {
+
     const initialShows = [];
     const [shows, setShows] = useState(initialShows);
     const initialText = '';
     const [text, setText] = useState(initialText);
-    console.log(shows);
 
     const fetchData = async () => {
-      const response = await axiosAPI.get();
+      const response = await axiosAPI.get(text);
       setShows(response.data);
+    };
+
+    const inputChangeHandler = (e) => {
+        fetchData();
+        setText(e.target.value);
     };
 
     useEffect( () => {
         fetchData();
-    }, []);
+    }, [text]);
+
+    const changeSelection = () => {
+        Object.keys(shows).map(name => {
+            setText(shows[name].show.name);
+        })
+    };
 
   return (
       <div className="App">
@@ -27,18 +39,25 @@ const App = () => {
                   <p className="main-title">Search for TV Show</p>
               </div>
               <div>
-                  <input type="text" onChange={(e) => setText(e.target.value)} value={text}/>
+                  <input type="text" onChange={inputChangeHandler} value={text}/>
+                      {Object.keys(shows).map(name => (
+                              <div key={nanoid()} className="selection" onClick={changeSelection}>{shows[name].show.name}</div>
+                      ))}
               </div>
           </div>
-                <div className="blocks">
-              {Object.keys(shows).map(k => (
-                  <div key={k}>
-                    <div className="block">
-                       <p>{shows[k].show.name}</p>
-                      <img src={shows[k].show.image.medium} alt="images"/>
-                    </div>
-                  </div>
-              ))}
+                <div className='blocks'>
+                    {Object.keys(shows).map(name => (
+                        <div key={name} className="block">
+                            <div>
+                                <img src={shows[name].show.image && shows[name].show.image.medium} alt="images"/>
+                            </div>
+                            <div>
+                            <p className="main-text">{shows[name].show.name}</p>
+                            <p className="text">{shows[name].show.summary}
+                            </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
       </div>
   )
